da6b67c6493ff950f64f94e58fe0edb20a53c132
Sean <shaddock@bokf.com>
2023-07-06 17:13:02 -0500

    calling login and cookie sent back but cookie not visible and not sent with sub request

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,10 +11,23 @@ export class AppComponent implements OnInit {
   result = '';
 
   ngOnInit(): void {
-    this.httpClient.get('http://localhost:3020/').subscribe((d) => {
-      console.log(d);
-      this.result = d.toString();
-    });
+    // this.httpClient.get('http://localhost:3020/').subscribe((d) => {
+    //   console.log(d);
+    //   this.result = d.toString();
+    // });
+
+    this.httpClient
+      .post('http://localhost:3020/login', {})
+      .subscribe((d: any) => {
+        console.log(d);
+        this.result = `${d.msg} - ${d.access_token}`;
+        this.httpClient
+          .get('http://localhost:3020/', { withCredentials: true })
+          .subscribe((d) => {
+            console.log(d);
+            this.result = d.toString();
+          });
+      });
   }
 
   title = 'client';
--- server/app.js
+++ server/app.js
@@ -16,6 +16,18 @@ app.get("/", (req, res) => {
   res.json("Hello World!");
 });
 
+app.post("/login", function (req, res, next) {
+  const access_token = "Bearer abc";
+  res.cookie("access_token", access_token, {
+    expires: new Date(Date.now() + 3600 * 1000 * 24 * 180 * 1), //second min hour days year
+    secure: true, // set to true if your using https or samesite is none
+    httpOnly: true, // backend only
+    sameSite: "none", // set to none for cross-request
+  });
+
+  res.json({ msg: "Login Successfully", access_token });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port} with CORS`);
 });
