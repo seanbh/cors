863f141511b37088720e065ab291b063555bece8
Sean <shaddock@bokf.com>
2023-07-06 16:52:44 -0500

    got cors working

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3020;
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port} with CORS`);
 });
